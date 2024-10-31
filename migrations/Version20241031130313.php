daa0aa54bc5314a18acbeae468833e66cf1f399e
Hamza BEJAOUI <hamzabejaoui2000@outlook.com>
2024-10-31 14:12:59 +0100

    Appartment entity

---
+++ migrations/Version20241031130313.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241031130313 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE appartment (id INT AUTO_INCREMENT NOT NULL, appartement_id BIGINT NOT NULL, type VARCHAR(255) NOT NULL, number_of_rooms INT NOT NULL, square_meters BIGINT NOT NULL, furnished TINYINT(1) NOT NULL, listing_id BIGINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE appartment');
+    }
+}
---
+++ src/Entity/Appartment.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AppartmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AppartmentRepository::class)]
+class Appartment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $appartementId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?int $numberOfRooms = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $squareMeters = null;
+
+    #[ORM\Column]
+    private ?bool $furnished = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $listingId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAppartementId(): ?string
+    {
+        return $this->appartementId;
+    }
+
+    public function setAppartementId(string $appartementId): static
+    {
+        $this->appartementId = $appartementId;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNumberOfRooms(): ?int
+    {
+        return $this->numberOfRooms;
+    }
+
+    public function setNumberOfRooms(int $numberOfRooms): static
+    {
+        $this->numberOfRooms = $numberOfRooms;
+
+        return $this;
+    }
+
+    public function getSquareMeters(): ?string
+    {
+        return $this->squareMeters;
+    }
+
+    public function setSquareMeters(string $squareMeters): static
+    {
+        $this->squareMeters = $squareMeters;
+
+        return $this;
+    }
+
+    public function isFurnished(): ?bool
+    {
+        return $this->furnished;
+    }
+
+    public function setFurnished(bool $furnished): static
+    {
+        $this->furnished = $furnished;
+
+        return $this;
+    }
+
+    public function getListingId(): ?string
+    {
+        return $this->listingId;
+    }
+
+    public function setListingId(string $listingId): static
+    {
+        $this->listingId = $listingId;
+
+        return $this;
+    }
+}
