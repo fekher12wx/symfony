975c7db90fda2d6ed6f004bdc87cb33ce107644c
Hamza BEJAOUI <hamzabejaoui2000@outlook.com>
2024-11-26 21:06:16 +0100

    correction security component

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,11 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,37 +15,26 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Name',
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('email', EmailType::class, [
+            ->add('email', TextType::class, [
                 'label' => 'Email',
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Password',
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('phone', TelType::class, [
-                'label' => 'Phone',
-                'attr' => ['class' => 'form-control']
+                'attr' => ['placeholder' => 'Enter email address'],
             ])
             ->add('roles', ChoiceType::class, [
                 'label' => 'Roles',
                 'choices' => [
                     'User' => 'ROLE_USER',
                     'Admin' => 'ROLE_ADMIN',
-                    'owner' => 'ROLE_OWNER',
+
                 ],
-                'multiple' => true,  // Allow selecting multiple roles
-                'expanded' => true,  // Display as checkboxes
-                'attr' => ['class' => 'form-control']
+
+                'expanded' => true,
+                'help' => 'Select one or more roles for the user',
             ])
-            ->add('save', SubmitType::class, [
-                'label' => 'Create User',
-                'attr' => ['class' => 'btn btn-primary']
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'attr' => ['placeholder' => 'Enter password'],
             ]);
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
